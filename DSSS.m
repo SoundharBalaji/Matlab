e3683978f9755ecf5096cc87702488fdadc10d58
Soundhar Balaji <131646222+SoundharBalaji@users.noreply.github.com>
2023-10-27 14:16:29 +0530

    Update DSSS.m

--- DSSS.m
+++ DSSS.m
@@ -1,74 +1,61 @@
 clc;
-close all;
 clear all;
-
-% Message signal
-msg = [1 0 1 0 0 1 1 0];
-n = length(msg);
-t = 0:0.02:n-0.02;
-
-% Create a perfect step waveform
-op = [];
-
-for i = 1:n
-    if msg(i) == 0
-        op = [op zeros(1, 50)];
-    elseif msg(i) == 1
-        op = [op ones(1, 50)];
+close all;
+b=round(rand(1,30));
+bitpattern=[];
+for k=1:30  
+   if b(1,k)==0
+       sig=-ones(1,20);
+   else
+       sig=ones(1,20);
+   end
+   bitpattern=[bitpattern sig]; 
+end 
+subplot(4,1,1);
+plot(bitpattern);  
+axis([-1 300 -1.5 1.5]);
+title('Original Bit Sequence');
+d=round(rand(1,120));
+pn_seq=[];
+carrier=[];
+t=[0:2*pi/4:2*pi];
+for k=1:120 
+    if d(1,k)==0 
+        sig=-ones(1,5);
+    else
+        sig=ones(1,5);
     end
+    c=cos(t);
+    carrier=[carrier c]; 
+    pn_seq=[pn_seq sig];
 end
-
-% Plot message signal
-subplot(2,2,1);
-plot(t, op);
-xlabel('Time');
-ylabel('Amplitude');
-title('Digital Message Signal');
-
-% Pesudo Noise sequence
-pn = [1 0 0 1 0 1 1 1];
-t_pn = 0:0.02:(n/10)-0.02;
-
-po = [];
-
-% Create a perfect step waveform
-for i = 1:n
-    if pn(i) == 0
-        po = [po zeros(1, 50)];
-    elseif pn(i) == 1
-        po = [po ones(1, 50)];
-    end
-end
-
-% Plot message signal
-subplot(2,2,2);
-plot(t_pn, po);
-xlabel('Time');
-ylabel('Amplitude');
-title('Pesudo Noise Signal');
-
-%spread code
-
-spc = msg .* pn;
-
-ok = [];
-
-% Create a perfect step waveform
-for i = 1:n
-    if spc(i) == 0
-        ok = [ok zeros(1, 50)];
-    elseif pn(i) == 1
-        ok = [ok ones(1, 50)];
-    end
-end
-
-% Plot message signal
-subplot(2,2,3);
-plot(ok);
-xlabel('Time');
-ylabel('Amplitude');
-title('Spread Signal');
-
-%carrier 
-
-
+spreading_sig=bitpattern.*pn_seq;
+subplot(4,1,2);
+plot(spreading_sig);
+axis([-1 300 -1.5 1.5]);
+title('Spreaded signal');
+bpsk_sig=spreading_sig.*carrier;
+subplot(4,1,3);
+plot(bpsk_sig);
+axis([-1 300 -1.5 1.5]); 
+title('BPSK Modulated Signal');
+figure
+rxsig=bpsk_sig.*carrier;
+demod_sig=[];
+for i=1:600
+    if rxsig(i)>=0
+        rxs=1;
+    else
+        rxs=-1;
+    end    
+    demod_sig=[demod_sig rxs];
+end 
+subplot(3,1,1);
+plot(demod_sig);
+axis([-1 300 -1.5 1.5]);
+title('Demodulated Signal');
+despread_sig=demod_sig.*pn_seq;
+subplot(3,1,2);
+plot(despread_sig);
+axis([-1 309 -1.5 1.5]);
+title('Despreaded data');
